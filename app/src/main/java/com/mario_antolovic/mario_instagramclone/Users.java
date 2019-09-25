5f72745c44a1b012d9757ac42cc855441719565d
zanerage <marioantolovic0@gmail.com>
2019-09-25 22:29:01 +0200

    users / user can see other users in the users tab

--- app/src/main/java/com/mario_antolovic/mario_instagramclone/Users.java
+++ app/src/main/java/com/mario_antolovic/mario_instagramclone/Users.java
@@ -6,12 +6,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Users extends Fragment {
+
+    private ListView listView;
+    private ArrayList arrayList;
+    private ArrayAdapter arrayAdapter;
+
 
 
     public Users() {
@@ -23,7 +38,34 @@ public class Users extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false);
+        View view =  inflater.inflate(R.layout.fragment_users, container, false);
+
+        listView = view.findViewById(R.id.listView);
+        arrayList = new ArrayList();
+        arrayAdapter = new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1,arrayList);
+
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+
+        parseQuery.whereNotEqualTo("username",ParseUser.getCurrentUser().getUsername());
+
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+                if (e ==null) {
+                    if (users.size()>0){
+                        for (ParseUser user: users){
+                            arrayList.add(user.getUsername());
+                        }
+                        listView.setAdapter(arrayAdapter);
+
+                    }
+                }
+            }
+        });
+
+
+
+        return view;
     }
 
 }
